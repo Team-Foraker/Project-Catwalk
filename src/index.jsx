a3f8a9446c1e1b2fa18b50f2c90a30aad4693ec6
Ben Black <benblack7@gmail.com>
2021-07-22 10:42:07 -0500

    Removed files

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Search from './Questions/Search/search.jsx';
-import Questions from './Questions/index.jsx';
-
-
-
-ReactDOM.render(<Questions/>, document.getElementById('questions'));
