793d9a198e5b2bfd361d5f749e61a583bf871f32
shannamurry <shanna.murry84@gmail.com>
2021-07-22 17:37:08 -0400

    restructured config and made shared component folder

---
+++ src/widgets/shared/Stars.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+const Stars = props => (
+
+  <div>
+    <span><FontAwesomeIcon icon={faStar} /></span>
+  </div>
+
+
+)
+
+export default Stars;
