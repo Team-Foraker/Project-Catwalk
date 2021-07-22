bce1d54acc129fa29fdab2faa3edca1ac1fcd380
June Lee <junehaklee94@gmail.com>
2021-07-22 11:17:50 +0900

    Changed RelatedItems to RelatedSection and implemented base structures of RelatedSection

---
+++ src/widgets/Related/RelatedSection.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const RelatedSection = (props) => {
+  const [products, setProducts] = useState({});
+
+  // fetches products data from Atelier API
+  // useEffect(() => {
+  //   axios.get('/products')
+  //        .then((res) => {
+  //          setProducts(res.data)
+  //        })
+  //        .catch((err) => {
+  //          throw err;
+  //        })
+  // }, [])
+
+  return (
+    <div>
+      Related Section will be here.
+    </div>
+  )
+
+}
+
+
+export default RelatedSection
