67b7e85cd7df86312d01027b644aa5297655c5bc
shannamurry <shanna.murry84@gmail.com>
2021-07-22 19:18:28 -0400

    working star rating component

---
+++ src/widgets/shared/StarRating.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import regular from '@fortawesome/fontawesome-free-regular';
+
+fontawesome.library.add(solid, regular);
+
+const StarRating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  return (
+    <div className="star-rating">
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        return (
+          <button
+            type="button"
+            key={index}
+            className={index <= (hover || rating) ? "on" : "off"}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            <span className="star">&#9733;</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
