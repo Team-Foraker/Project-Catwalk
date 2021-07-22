d4fd9b7368eca9441ea85aae2691e466b989f400
Joshua Barnes <josh.barnes003@gmail.com>
2021-07-22 16:14:36 -0400

    updated config.example.js to be more importable

--- config.example.js
+++ config.example.js
@@ -1,3 +1,4 @@
-const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/'
-
-const API_TOKEN = 'add your GitHub API token here'
+module.exports = {
+  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/',
+  API_TOKEN: 'add your GitHub API token here'
+};
