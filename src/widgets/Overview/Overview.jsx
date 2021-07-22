c5891fcac66cb691e05481441d5bf9146bc7d758
Joshua Barnes <josh.barnes003@gmail.com>
2021-07-22 18:52:59 -0400

    Set up Basic Product Info Framework

--- src/widgets/Overview/Overview.jsx
+++ src/widgets/Overview/Overview.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ProductInfo from './Components/ProductInfo.jsx'
+import axios from 'axios';
+const {url, API_TOKEN} = require('../../../config.js')
 
 var Overview = function(props) {
+
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    axios.get(url + 'products')
+      .then( (products) => {
+        console.log(`fetched data:`);
+        console.log(products.data[0])
+        setProduct(products.data[0])
+      })
+      .catch( (error) => {
+        console.log(error);
+      })
+  }, []);
+
   return (
-    <div>Overview Widget</div>
+    <div>
+      Overview
+      {/* Image Gallery */}
+      <ProductInfo product={product}/>
+      {/* Style Selector */}
+      {/* Add to Cart */}
+    </div>
   )
 }
 
