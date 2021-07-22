6b348a11e7151c8c2ac0d64b17da004866bbad6e
Ben Black <benblack7@gmail.com>
2021-07-22 11:19:44 -0500

    added questions

--- src/widgets/Questions/index.js
+++ src/widgets/Questions/index.js
@@ -1,1 +1,24 @@
-first file
+import React from 'react';
+//import Search from './Search/search.jsx';
+
+class Questions extends React.Component {
+  constructor(props)
+    super(props)
+}
+
+render() {
+  return (
+    <div>
+      <h1>QUESTIONS & ANSWERS</h1>
+      <div><Search /></div>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Questions;
