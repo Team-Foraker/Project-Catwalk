41fc2493d5b160b838072bbb0787223da2ec4e08
Joshua Barnes <josh.barnes003@gmail.com>
2021-07-23 10:55:45 -0400

    Added StyleSelector Component with basic rendering

--- src/widgets/Overview/Overview.jsx
+++ src/widgets/Overview/Overview.jsx
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import ProductInfo from './Components/ProductInfo.jsx'
+import StyleSelector from './Components/StyleSelector.jsx'
 import axios from 'axios';
 const {url, API_TOKEN} = require('../../../config.js')
 
 var Overview = function(props) {
 
   const [product, setProduct] = useState({});
+  const [styles, setStyles] = useState([]);
 
   useEffect(() => {
     axios.get(url + 'products')
@@ -13,6 +15,14 @@ var Overview = function(props) {
         console.log(`fetched data:`);
         console.log(products.data[0])
         setProduct(products.data[0])
+        axios.get(`${url}products/${products.data[0].id}/styles`)
+          .then( (styles) => {
+            console.log(styles.data.results);
+            setStyles(styles.data.results);
+          })
+          .catch( (error) => {
+            console.log(error)
+          })
       })
       .catch( (error) => {
         console.log(error);
@@ -24,7 +34,7 @@ var Overview = function(props) {
       Overview
       {/* Image Gallery */}
       <ProductInfo product={product}/>
-      {/* Style Selector */}
+      <StyleSelector styles={styles}/>
       {/* Add to Cart */}
     </div>
   )
