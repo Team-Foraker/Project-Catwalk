a7f4b885f8d42d6997b73ca27c6b223cf854dcd8
Joshua Barnes <josh.barnes003@gmail.com>
2021-07-23 13:48:14 -0400

    Created new Style Component to break down Selector further

--- src/widgets/Overview/Components/StyleSelector.jsx
+++ src/widgets/Overview/Components/StyleSelector.jsx
@@ -1,15 +1,17 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-
-const {url, API_TOKEN} = require('../../../../config.js')
+import Style from './Style.jsx';
 
 const StyleSelector = function({styles}) {
 
+  // will need a click handler, that hooks back to overview to disperse new selected style state down to other components
+  // will also need to handle AJAX requests for the selected style's info from the Database
+
   return (
     <div>
+      <span>Style > Selected Style</span>
       {styles.map( (style) => {
         return (
-          <div key={style.style_id}>{style.name}</div>
+          <Style key={style.style_id} style={style}/>
         )
       })}
     </div>
