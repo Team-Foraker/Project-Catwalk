8948426696223b338022daaeadf6c6124d770e79
Ben Black <benblack7@gmail.com>
2021-07-26 17:50:51 -0500

    Only display first two Answers

--- src/widgets/Questions/QAList.jsx
+++ src/widgets/Questions/QAList.jsx
@@ -11,6 +11,7 @@ const params = {
 
 const QAList = () => {
   const [questions, setQuestions] = useState([]);
+  const [answersShown, setAnswersShown] = useState(2);
   useEffect(() => {
     axios.get(url + 'qa/questions', { params: params })
       .then((res) => {
@@ -25,11 +26,11 @@ const QAList = () => {
     <div>
       <h3>List of Questions</h3>
       <ul>
-       {questions.map((question) => (
-        <div key={question.question_id}>
-           <h4>Q: {question.question_body} </h4>
-           {Object.values(question.answers).map((answers) => (
-             <div key={answers.id}>
+        {questions.map((question) => (
+          <div key={question.question_id}>
+            <h4>Q: {question.question_body} </h4>
+            {Object.values(question.answers).slice(0, answersShown).map((answers) => (
+              <div key={answers.id}>
                 <h5>A: {answers.body} </h5>
                 <div class="answers_details">
                   <div class="useranswer">by {answers.answerer_name}, <Moment format='MMMM Do YYYY'>{answers.date}</Moment></div>
@@ -37,12 +38,21 @@ const QAList = () => {
                   <div class="answeryes">Yes ({answers.helpfulness})</div>
                   <div class="report">Report</div>
                 </div>
-             </div>
-           ))}
+              </div>
+            ))}
+            <div>
+              <div>Helpful?</div>
+              <div>Yes ({question.question_helpfulness})</div>
+              <div>Add Answer</div>
+            </div>
           </div>
         ))}
-    </ul>
-   </div>
+        <div>
+          <button class="qabutton" onClick={() => setAnswersShown(answersShown + 2)}>MORE ANSWERED QUESTIONS</button>
+          <button class="qabutton">ADD A QUESTION +</button>
+        </div>
+      </ul>
+    </div>
   )
 }
 
