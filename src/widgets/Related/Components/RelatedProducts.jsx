34ab96d5de0245796093dc2904efc47e39b466fc
June Lee <junehaklee94@gmail.com>
2021-07-28 02:55:22 +0900

    Implemented RelatedProducts carousel

--- src/widgets/Related/Components/RelatedProducts.jsx
+++ src/widgets/Related/Components/RelatedProducts.jsx
@@ -5,6 +5,24 @@ axios.defaults.headers.common['Authorization'] = API_TOKEN;
 
 const RelatedProducts = (props) => {
   const [relatedProducts, setRelatedProducts] = useState([]);
+  const [leftCount, setLeftCount] = useState(0);
+  const [rightCount, setRightCount] = useState(relatedProducts.length);
+
+  // leftCount
+  const increment = () => {
+    setLeftCount((leftCount) => leftCount + 1)
+  }
+
+  // rightCount
+  const decrement = () => {
+    setLeftCount((leftCount) => {
+      if (leftCount !== 0) {
+        return leftCount - 1;
+      } else {
+        return 0;
+      }
+    })
+  }
 
 
   const getRelatedProducts = (arrayProductID) => {
@@ -28,14 +46,17 @@ const RelatedProducts = (props) => {
   useEffect(() => {
     axios.get(url + 'products')
          .then((res) => {
-           axios.get(url + `products/${res.data[0].id}/related`)
+           axios.get(url + `products/${res.data[1].id}/related`)
            .then((res) => {
              //res.data = [12030, 230124, 412033, 23013]
              getRelatedProducts(res.data)
              .then((res) => {
+               // res === array of related products
+
                let arrayRP = res.map((rp) => {
                  return rp.data;
                });
+               console.log(arrayRP)
                return arrayRP;
              })
              .then((arrayRP) => {
@@ -52,6 +73,7 @@ const RelatedProducts = (props) => {
                })
                .then((res) => {
                  setRelatedProducts(res);
+                 setRightCount(arrayRP.length);
                })
              })
            })
@@ -59,23 +81,34 @@ const RelatedProducts = (props) => {
   },[])
 
 
-
   return (
     <div>
       <h3>Related Products Section</h3>
-      <div>
-        {relatedProducts.map((item, index) =>
-        <div key={index} style={{border: '1px solid black', width: '20%', float: 'left', margin: '3px', height: '250px'}}>
+      <div className='relatedCards' style={{display: 'flex', flexDirection: 'row', width: 'fitContent', position: 'relative'}}>
 
+        {leftCount !== 0 ? (
+          <i className='leftArrow' style={{position: 'absolute', top: '50%', width: '0', height: '0', borderTop: '15px solid transparent', borderBottom: '15px solid transparent', borderRight: '15px solid red'}} onClick={() => {
+            decrement();
+          }}>
+          </i>
+        ) : ( null )}
+
+        {relatedProducts.slice(leftCount, rightCount).map((item, index) =>
+        <div key={index} style={{border: '1px solid black', width: '20%', margin: '3px', height: '250px'}}>
         <img style={{height: '60%', width: '100%'}} src={item.results[0].photos[0].thumbnail_url}/>
         <div>{item.category}</div>
         <div style={{fontWeight: 'bold'}}>{item.name}</div>
         <div>{item.default_price}</div>
         <div>StarRating</div>
-
         </div>
         )}
 
+        {leftCount !== rightCount - 1 ? (
+          <i className='rightArrow' style={{position: 'absolute', top: '50%', right: '10%', width: '0', height: '0', borderTop: '15px solid transparent', borderBottom: '15px solid transparent', borderLeft: '15px solid red'}} onClick={() => {
+            increment();
+          }}>
+          </i>
+        ) : ( null )}
 
       </div>
 
@@ -83,5 +116,6 @@ const RelatedProducts = (props) => {
     </div>
   )
 }
+
 
 export default RelatedProducts
