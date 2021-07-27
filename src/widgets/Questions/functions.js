23c31a7b39e3a104f58092903fe438e3067cadb0
Ben Black <benblack7@gmail.com>
2021-07-27 18:20:59 -0500

    functins

--- src/widgets/Questions/functions.js
+++ src/widgets/Questions/functions.js
@@ -1,27 +1,37 @@
-const {url, API_TOKEN} = require('../../../config.js');
+const { url, API_TOKEN } = require('../../../config.js');
 const axios = require('axios');
 axios.defaults.headers.common['Authorization'] = API_TOKEN;
-/*
-
-to use set up your axios using files like so:
-const {url, API_TOKEN} = require('../config.js');
-axios.defaults.headers.common['Authorization'] = API_TOKEN;
-
-then:
-axios.get(url + '/<insert route>')
-
-*/
 
 module.exports = {
-
-  getQuestions: function () {
-    axios.get(url + '/qa/questions')
-    .then((res) => {
-      console.log(res.data)
-      return res.data;
-    })
-    .catch((err) => {
-      console.log("Error in Get: ", err);
-    })
+  sortAnswers: function (a, b) {
+    const answerA = a.helpfulness;
+    const answerB = b.helpfulness;
+    let comparison = 0;
+    if (answerA > answerB) {
+      comparison = -1;
+    } else {
+      comparison = 1;
+    }
+    return comparison;
+  },
+  handleQuestionYes: (e) => {
+    let question = e.target.id;
+    axios.put(url + `qa/questions/${question}/helpful`)
+      .then((res) => {
+        console.log("Question helpfullness updated", res);
+      })
+      .catch((err) => {
+        console.log("Question helpfullness not updated", err);
+      })
+  },
+  handleAnswerYes: (e) => {
+    let answer = e.target.id;
+    axios.put(url + `qa/answers/${answer}/helpful`)
+      .then((res) => {
+        console.log("Answer helpfullness updated", res);
+      })
+      .catch((err) => {
+        console.log("Answer helpfullness not updated", err);
+      })
   }
 }
