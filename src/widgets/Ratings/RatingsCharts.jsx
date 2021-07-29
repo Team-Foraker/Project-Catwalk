a49a111f47cc56c6d3e1e1f46d5d4be485bbf9dd
shannamurry <shanna.murry84@gmail.com>
2021-07-29 14:05:19 -0400

    implemented bare bones layout

--- src/widgets/Ratings/RatingsCharts.jsx
+++ src/widgets/Ratings/RatingsCharts.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 const RatingsCharts = props => (
   <div className="ratings-charts-container">
     Ratings Charts here
+    <div>average rating here</div>
+    <div>percent recommended here</div>
+    <div>stars bar graphs here</div>
+    <div>size breakdown here</div>
+    <div>comfort breakdown here</div>
   </div>
 )
 
