80d0452ebd84a10a4d01e26e03463f8cd8e41812
shannamurry <shanna.murry84@gmail.com>
2021-07-29 16:13:09 -0400

    average ratings component

--- src/widgets/Ratings/AverageStars.jsx
+++ src/widgets/Ratings/AverageStars.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const AverageStars = props => {
+  var ratings = props.ratings;
+
+  const [averageRating, setAverageRating] = useState(0);
+
+  useEffect(() => {
+    var totalReviews = 0;
+    var totalStars = 0;
+    for (var key in ratings) {
+      totalReviews += parseInt(ratings[key]);
+      totalStars += parseInt(ratings[key]) * key;
+    }
+    var average = totalStars/totalReviews;
+    setAverageRating(average.toFixed(1));
+  })
+
   return(
-    <div>
-      Average Stars Component
+    <div className="averageRating-flex-container">
+      <h1>{averageRating}</h1>
+      <span>&#9734;&#9734;&#9734;&#9734;&#9734;</span>
     </div>
   )
 }
