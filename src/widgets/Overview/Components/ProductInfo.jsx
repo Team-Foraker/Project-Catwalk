4ba5f6bbe182abaff6905687225d66daef9949e8
Joshua Barnes <josh.barnes003@gmail.com>
2021-07-29 13:44:08 -0400

    fixed bug, needs pull

--- src/widgets/Overview/Components/ProductInfo.jsx
+++ src/widgets/Overview/Components/ProductInfo.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import StarRating from '../../shared/StarRating.jsx';
 
-const ProductInfo = function({product, style}) {
+const ProductInfo = function({product, currentStyle}) {
   return (
     <div>
       <StarRating />
       {/* Reviews link */}
       <h3>{product.category}</h3>
       <h2>{product.name}</h2>
-      {style.sale_price
-      ? <span><s>${style.original_price}</s>  <span style={ {'color': 'red'}}>${style.sale_price}</span></span>
-      : <span>${style.original_price}</span>}
+      {currentStyle.sale_price
+      ? <span><s>${currentStyle.original_price}</s>  <span style={ {'color': 'red'}}>${currentStyle.sale_price}</span></span>
+      : <span>${currentStyle.original_price}</span>}
     </div>
   )
 }
