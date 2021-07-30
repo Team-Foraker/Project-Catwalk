5a539a06d1e5b7dd0e6ba01b24bb636e4d993f91
Joshua Barnes <josh.barnes003@gmail.com>
2021-07-30 16:04:06 -0400

    All non-css portion of style is complete

--- src/widgets/Overview/Components/StyleSelector.jsx
+++ src/widgets/Overview/Components/StyleSelector.jsx
@@ -3,14 +3,22 @@ import Style from './Style.jsx';
 
 const StyleSelector = function({styles, currentStyle, setCurrent}) {
 
+  const gridStyle = {
+    'display': 'grid',
+    'gridTemplateColumns': '65px 65px 65px 65px',
+    'gridGap': '15px'
+  }
+
   return (
     <div>
       <h4><b>Style ></b> {currentStyle.name}</ h4>
+      <div style={gridStyle}>
       {styles.map( (style) => {
         return (
-          <Style key={style.style_id} style={style} setCurrent={setCurrent} />
+          <Style key={style.style_id} style={style} setCurrent={setCurrent} currentStyle={currentStyle} />
         )
       })}
+      </div>
     </div>
   )
 }
