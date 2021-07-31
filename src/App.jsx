3c1f86ed9815a8d2df7b2dbce01a32c9ac3767de
June Lee <junehaklee94@gmail.com>
2021-07-31 18:45:58 -0500

    Added axios get request for products

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Ratings from './widgets/Ratings/Ratings.jsx';
 import Overview from './widgets/Overview/Overview.jsx';
 import RelatedSection from './widgets/Related/RelatedSection.jsx';
 import Questions from './widgets/Questions/Questions.jsx';
+const { url, API_TOKEN } = require('/config.js');
+axios.defaults.headers.common['Authorization'] = API_TOKEN;
 
 const App = (props) => {
+
+  const [getProducts, setGetProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get(url + 'products')
+      .then((res) => {
+        setGetProducts(res.data[1])
+      }
+      )
+  }, [])
+
+
+
+
   return (<div>
     <Overview />
     <RelatedSection />
