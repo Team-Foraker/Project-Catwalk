cdd67fb7c6e0eb74ef8d545dcf9b167d600fe88f
shannamurry <shanna.murry84@gmail.com>
2021-08-01 19:39:29 -0400

    star chart working

--- src/widgets/shared/AverageStarRating.jsx
+++ src/widgets/shared/AverageStarRating.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const AverageStarRating = props => {
+  var average = (props.average * 16).toFixed(2);
+
   return (
     <div className="star-chart-container">
       <div className="hollow-stars">
@@ -10,7 +12,7 @@ const AverageStarRating = props => {
         <span>&#9734;</span>
         <span>&#9734;</span>
       </div>
-      <div className="solid-stars">
+      <div className="solid-stars" style={{width: `${average}px`}}>
         <span>&#9733;</span>
         <span>&#9733;</span>
         <span>&#9733;</span>
