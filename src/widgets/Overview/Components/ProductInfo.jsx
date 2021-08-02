f6ca394348b8c0fa6a298340adbb0b693b07641c
Joshua Barnes <josh.barnes003@gmail.com>
2021-08-02 10:13:02 -0400

    Added Average Star Rating component to Product Info

--- src/widgets/Overview/Components/ProductInfo.jsx
+++ src/widgets/Overview/Components/ProductInfo.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import StarRating from '../../shared/StarRating.jsx';
 import SocialMedia from './SocialMedia.jsx';
+import AverageStarRating from '../../shared/AverageStarRating.jsx';
 import axios from 'axios';
 const {url, API_TOKEN} = require('../../../../config.js')
 axios.defaults.headers.common['Authorization'] = API_TOKEN;
@@ -8,6 +9,7 @@ axios.defaults.headers.common['Authorization'] = API_TOKEN;
 const ProductInfo = function({product, currentStyle}) {
 
   const [reviewCount, setReviewCount] = useState(0);
+  const [average, setAverage] = useState(0);
 
   useEffect( () => {
     if (product.id) {
@@ -15,10 +17,14 @@ const ProductInfo = function({product, currentStyle}) {
         .then( (results) => {
           let ratings = Object.keys(results.data.ratings);
           let reviews = 0;
+          var totalStars = 0;
           for (var i = 0; i < ratings.length; i++) {
             reviews += JSON.parse(results.data.ratings[ratings[i]]);
+            totalStars += JSON.parse(results.data.ratings[ratings[i]]) * ratings[i]
           }
           setReviewCount(reviews)
+          let unroundedAverage = totalStars/reviews
+          setAverage(unroundedAverage.toFixed(1))
         })
         .catch( (error) => {
           throw new Error(error);
@@ -34,10 +40,12 @@ const ProductInfo = function({product, currentStyle}) {
 
   return (
     <div style={productStyle}>
-      <StarRating />
+      <div style={ {'display': 'grid'} }>
+      <AverageStarRating average={average} />
       {reviewCount
-      ? <span><a href="#ratings">Read all {reviewCount} reviews</a></span>
+      ? <span style={ {'gridColumnStart': '2'} }><a href="#ratings">Read all {reviewCount} reviews</a></span>
       : <span></span>}
+      </div>
       <h3>{product.category}</h3>
       <h2>{product.name}</h2>
       {currentStyle.sale_price
