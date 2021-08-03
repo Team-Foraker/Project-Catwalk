40d197aba4ec7248a45b4210f7f9d92ce3de13f5
June Lee <junehaklee94@gmail.com>
2021-08-03 14:37:59 -0500

    Implemented star rating on related product cards

--- src/widgets/Related/Components/RelatedProducts.jsx
+++ src/widgets/Related/Components/RelatedProducts.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import RelatedModal from './RelatedModal.jsx'
+import AverageStarRating from '../../shared/AverageStarRating.jsx';
 const { url, API_TOKEN } = require('../../../../config.js');
 axios.defaults.headers.common['Authorization'] = API_TOKEN;
 
@@ -11,6 +12,8 @@ const RelatedProducts = (props) => {
   const [showModal, setShowModal] = useState(false);
   const [compareItems, setCompareItems] = useState([]);
   const [currentProduct, setCurrentProduct] = useState([]);
+  const [reviewCount, setReviewCount] = useState([]);
+  const [average, setAverage] = useState([]);
 
 
   // leftCount
@@ -48,45 +51,70 @@ const RelatedProducts = (props) => {
     return Promise.all(promiseStyles);
   }
 
+  const getStarRatings = (arrayProductID) => {
+    const promise = arrayProductID.map((id) => {
+      return axios.get(url + `reviews/meta?product_id=${id}`)
+        .then()
+        .catch();
+    })
+    return Promise.all(promise);
+  }
 
   useEffect(() => {
     if (props.getProducts.id) {
-        axios.get(url+ `products/${props.getProducts.id}`)
-             .then((res) => {
-               setCurrentProduct(res.data)
-             })
-        axios.get(url + `products/${props.getProducts.id}/related`)
-          .then((res) => {
-            //res.data = [12030, 230124, 412033, 23013]
-            getRelatedProducts(res.data)
-              .then((res) => {
-
-                // res === array of related products
-
-                let arrayRP = res.map((rp) => {
-                  return rp.data;
-                });
-                return arrayRP;
-              })
-              .then((arrayRP) => {
-                let productID = arrayRP.map((rp) => {
-                  return rp.id;
-                });
-                getRelatedStyles(productID)
-                  .then((styleData) => {
-                    let arrayStyle = styleData.map((rp, index) => {
-                      let temp = Object.assign({}, rp.data, arrayRP[index]);
-                      return temp;
-                    });
-                    return arrayStyle;
-                  })
-                  .then((res) => {
-                    setRelatedProducts(res);
-                    setRightCount(arrayRP.length);
-                  })
-              })
-          })
-        }
+      axios.get(url + `products/${props.getProducts.id}`)
+        .then((res) => {
+          setCurrentProduct(res.data)
+        })
+      axios.get(url + `products/${props.getProducts.id}/related`)
+        .then((res) => {
+          getStarRatings(res.data)
+            .then((res) => {
+              let individualRating = res.map((indv) => {
+                return indv.data.ratings
+              });
+              return individualRating;
+            })
+            .then((res) => {
+              let indivdualCalculations = res.map((cal) => {
+                let ratings = Object.keys(cal);
+                let reviews = 0;
+                let totalStars = 0;
+                for (let i = 0; i < ratings.length; i++) {
+                  reviews += JSON.parse(cal[ratings[i]])
+                  totalStars += JSON.parse(cal[ratings[i]]) * ratings[i]
+                }
+                let unroundedAverage = totalStars / reviews
+                return unroundedAverage.toFixed(1)
+              });
+              setAverage(indivdualCalculations);
+            });
+          getRelatedProducts(res.data)
+            .then((res) => {
+              let arrayRP = res.map((rp) => {
+                return rp.data;
+              });
+              return arrayRP;
+            })
+            .then((arrayRP) => {
+              let productID = arrayRP.map((rp) => {
+                return rp.id;
+              });
+              getRelatedStyles(productID)
+                .then((styleData) => {
+                  let arrayStyle = styleData.map((rp, index) => {
+                    let temp = Object.assign({}, rp.data, arrayRP[index]);
+                    return temp;
+                  });
+                  return arrayStyle;
+                })
+                .then((res) => {
+                  setRelatedProducts(res);
+                  setRightCount(arrayRP.length);
+                })
+            })
+        })
+    }
   }, [props.getProducts])
 
   return (
@@ -102,23 +130,24 @@ const RelatedProducts = (props) => {
 
         {relatedProducts.slice(leftCount, rightCount).map((item, index) => {
           if (index < 4) {
-          return (<div key={index} className='related-products'>
-            <button onClick={() => {
-            setShowModal(true);
-            setCompareItems(item);}}>Modal Button</button>
-            <img className='related-image' src={item.results[0].photos[0].thumbnail_url} />
-            <div className='related-category'>{item.category}</div>
-            <div className='related-name'>{item.name}</div>
-            {item.sale_price ? (
-            <div>
-              <s className='related-price' style={{color: 'red'}}>${item.default_price}</s>
-              <div className='related-price'>${item.sale_price}</div>
+            return (<div key={index} className='related-products'>
+              <button onClick={() => {
+                setShowModal(true);
+                setCompareItems(item);
+              }}>Modal Button</button>
+              <img className='related-image' src={item.results[0].photos[0].thumbnail_url} />
+              <div className='related-category'>{item.category}</div>
+              <div className='related-name'>{item.name}</div>
+              {item.sale_price ? (
+                <div>
+                  <s className='related-price' style={{ color: 'red' }}>${item.default_price}</s>
+                  <div className='related-price'>${item.sale_price}</div>
+                </div>
+              ) : (<div className='related-price'>${item.default_price}</div>)}
+              <AverageStarRating average={average[index]} />
+
             </div>
-            ) : (<div className='related-price'>${item.default_price}</div>)}
-            <div className='related-star'>StarRating</div>
-            
-          </div>
-          )
+            )
           }
         }
         )}
@@ -130,7 +159,7 @@ const RelatedProducts = (props) => {
           </i>
         ) : (null)}
 
-        <RelatedModal getProducts={props.getProducts} showModal={showModal} onClose={() => setShowModal(false)} chosenItem={compareItems} currentProduct={currentProduct}/>
+        <RelatedModal getProducts={props.getProducts} showModal={showModal} onClose={() => setShowModal(false)} chosenItem={compareItems} currentProduct={currentProduct} />
 
       </div>
     </div>
