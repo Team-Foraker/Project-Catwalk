11d104d25b1026f72a4415c0c55f3922d6de3b1e
Joshua Barnes <josh.barnes003@gmail.com>
2021-08-04 11:54:03 -0400

    style selector and general styling basics complete

--- src/widgets/Overview/Components/Checkmark.jsx
+++ src/widgets/Overview/Components/Checkmark.jsx
@@ -2,30 +2,8 @@ import React from 'react';
 
 const Checkmark = function(props) {
 
-  const checkmarkHolder = {
-    'height': '15px',
-    'width': '15px',
-    'borderRadius': '50%',
-    'borderStyle': 'solid',
-    'borderWidth': 'thin'
-  }
-
-  const leftMark = {
-    'height': '2px',
-    'width': '5px',
-    'backgroundColor': 'black',
-    'transform': 'rotate(45deg)'
-  }
-
-  const rightMark = {
-    'height': '11px',
-    'width': '2px',
-    'backgroundColor': 'black',
-    'transform': 'rotate(225deg)'
-  }
-
   return (
-    <div style={checkmarkHolder}><div style={leftMark}></div><div style={rightMark}></div></div>
+    <div className="checkmark-holder"><div className="left-mark"></div><div className="right-mark"></div></div>
   )
 }
 
--- src/widgets/Overview/Components/Style.jsx
+++ src/widgets/Overview/Components/Style.jsx
@@ -3,16 +3,8 @@ import Checkmark from './Checkmark.jsx';
 
 const Style = function({style, currentStyle, setCurrent}) {
 
-  const imgStyle = {
-    'height': '65px',
-    'width': '65px',
-    'borderRadius': '50%',
-    'borderStyle': 'solid',
-    'borderWidth': 'thin'
-  }
-
   return (
-    <div>
+    <div className="style-holder" >
       { style.style_id === currentStyle.style_id
       ? <React.Fragment><img className="thumbnail" src={style.photos[0].thumbnail_url} onClick={() => {setCurrent(style)}} /><Checkmark /></React.Fragment>
       : <img className="thumbnail" src={style.photos[0].thumbnail_url} onClick={() => {setCurrent(style)}} />}
